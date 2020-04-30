95319dc1fd511bc7edd955dd66feb061b290777e
sortheish <you@example.com>
2020-04-30 16:52:13 +0530

    Added Create user UI

---
+++ app/src/main/java/com/example/smackchat/CreateUserActivity.kt
@@ -0,0 +1,23 @@
+package com.example.smackchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatarClick(view:View){
+
+    }
+    fun genarateBackgroundColorClick(view:View){
+
+    }
+    fun createNewUserClick(view: View){
+
+    }
+}
