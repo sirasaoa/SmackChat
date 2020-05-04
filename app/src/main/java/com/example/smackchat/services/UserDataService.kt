0e0aaefa2cd3c4429649cc5a5f7777c21871b3a0
sortheish <you@example.com>
2020-05-04 13:33:25 +0530

    Added the logIn/logOut funtionality, polish CreateUserActivity

--- app/src/main/java/com/example/smackchat/services/UserDataService.kt
+++ app/src/main/java/com/example/smackchat/services/UserDataService.kt
@@ -1,9 +1,43 @@
 package com.example.smackchat.services
 
+import android.graphics.Color
+import java.util.*
+
 object UserDataService {
     var id =""
     var avatarColor = ""
     var avatarName = ""
     var email =""
     var name = ""
+
+    fun logout(){
+        id =""
+        avatarColor = ""
+        avatarName = ""
+        email =""
+        name = ""
+        AuthService.authToken = ""
+        AuthService.userEmail = ""
+        AuthService.isLoggedIn = false
+    }
+    fun returnAvatarColor(components:String):Int{
+        //[1.3456789345, 1.548484784, 2.8848383000, 1]
+        //1.3456789345 1.548484784 2.8848383000 1
+        val strippedColor = components
+                             .replace("[","")
+                             .replace("]","")
+                             .replace(",","")
+
+        var r = 0
+        var g = 0
+        var b = 0
+
+        val scanner = Scanner(strippedColor)
+        if(scanner.hasNext()){
+            r = (scanner.nextDouble() * 255).toInt()
+            g = (scanner.nextDouble() * 255).toInt()
+            b = (scanner.nextDouble() * 255).toInt()
+        }
+         return Color.rgb(r,g,b)
+    }
 }
--- app/src/main/java/com/example/smackchat/ui/CreateUserActivity.kt
+++ app/src/main/java/com/example/smackchat/ui/CreateUserActivity.kt
@@ -1,12 +1,15 @@
 package com.example.smackchat.ui
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.smackchat.R
 import com.example.smackchat.services.AuthService
+import com.example.smackchat.utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -16,6 +19,8 @@ class CreateUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+
+        createSpinner.visibility = View.INVISIBLE
     }
 
     fun generateUserAvatarClick(view:View){
@@ -47,21 +52,52 @@ class CreateUserActivity : AppCompatActivity() {
         avatarColor = "[$saveR, $saveG, $saveB, 1]"
     }
     fun createNewUserClick(view: View){
+        enableSpinner(true)
         val userName= createUserNameText.text.toString()
         val email = createEmailText.text.toString()
         val password: String = createPasswordText.text.toString()
-       AuthService.registerUser(this,email,password){ registerSuccess->
-           if(registerSuccess){
-              AuthService.loginUser(this,email,password){loginSuccess->
-                  if(loginSuccess){
-                      AuthService.crateUser(this,userName,email,userAvatar,avatarColor){crateSuccess->
-                          if(crateSuccess){
-                              finish()
-                          }
-                      }
-                  }
-              }
-           }
-       }
+        if(userName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()){
+            AuthService.registerUser(this,email,password){ registerSuccess->
+                if(registerSuccess){
+                    AuthService.loginUser(this,email,password){loginSuccess->
+                        if(loginSuccess){
+                            AuthService.crateUser(this,userName,email,userAvatar,avatarColor){crateSuccess->
+                                if(crateSuccess){
+                                    val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                                    LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+                                    enableSpinner(false)
+                                    finish()
+                                }else{
+                                    errorToast()
+                                }
+                            }
+                        }else{
+                            errorToast()
+                        }
+                    }
+                }else{
+                    errorToast()
+                }
+        }
+       }else{
+            Toast.makeText(this,"Make sure user name, email, password are filled in",Toast.LENGTH_LONG).show()
+            enableSpinner(false)
+        }
+    }
+
+    private fun errorToast(){
+        Toast.makeText(this,"Something went wrong, please try again",Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(enable:Boolean){
+        if(enable) {
+            createSpinner.visibility = View.VISIBLE
+        }else{
+            createSpinner.visibility = View.INVISIBLE
+        }
+        btnCreateUser.isEnabled = !enable
+        btnBackgoundColor.isEnabled = !enable
+        createAvatarImageView.isEnabled = !enable
     }
 }
