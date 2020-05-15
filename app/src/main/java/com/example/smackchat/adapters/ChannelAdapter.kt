d3539c6b3efea676df3f8e646dfddb8c6d956d28
sortheish <63228602+sortheish@users.noreply.github.com>
2020-05-15 19:01:12 +0530

    Revert test changes
    Revert test changes

--- app/src/main/java/com/example/smackchat/adapters/ChannelAdapter.kt
+++ app/src/main/java/com/example/smackchat/adapters/ChannelAdapter.kt
@@ -43,12 +43,10 @@ class ChannelAdapter(context : Context, channels :MutableList<Channel> ): BaseAd
     }
 
     fun updateItemList(channelList:ArrayList<Channel>){
-        if(true){
        if(channelList.isNotEmpty()){
            this.channels.clear()
            this.channels.addAll(channelList)
        }
-        }
         notifyDataSetChanged()
     }
 
